926ef859e234e283a2e5ee6e5149d63e938c373b
mouhamedaminetroudi <57112126+Troudi27@users.noreply.github.com>
2024-12-30 20:21:55 +0100

    In this exercise, you will practice assigning values to variables and performing simple operations with them. Variables are used to store data that can be used or manipulated later in your code.

---
+++ Basic-variable-assignment/test.py
@@ -0,0 +1,18 @@
+
+# Exercise Note: Basic Variable Assignment
+
+# In this exercise, you will practice assigning values to variables and performing simple operations with them. Variables are used to store data that can be used or manipulated later in your code.
+
+# Examples:
+# Assigning Values
+
+# python
+# Copy code
+# # Example of assigning values to variables
+# name = "Alice"
+# age = 25
+
+
+a = "code"
+b = "wa.rs"
+name = a + b
